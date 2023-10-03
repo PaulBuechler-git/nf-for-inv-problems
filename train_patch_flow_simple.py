cd367ee42deff6c91af0acb561470e8e67d10aed
Paul Büchler <paul.buechler@tuhh.de>
2023-10-03 19:49:58 +0200

    changed checkpoint saving

--- train_patch_flow_simple.py
+++ train_patch_flow_simple.py
@@ -34,7 +34,7 @@ def main(prop_args):
 
     model = PatchFlow(input_dims=img_dims, multiscale_blocks=multiscale_blocks, block_depth=block_depth, hidden_channels=hidden_channels)
     logger = TensorBoardLogger("tb_logs", name="patch_flow_simple")
-    ckpt_callback = pl.callbacks.ModelCheckpoint(save_top_k=5, every_n_train_steps=100)
+    ckpt_callback = pl.callbacks.ModelCheckpoint(save_top_k=-1, every_n_train_steps=1000)
     trainer = pl.Trainer(accelerator="gpu" if torch.cuda.is_available() else "cpu", max_epochs=epoc, logger=logger, callbacks=[ckpt_callback])
     trainer.fit(model, train_loader)
 
