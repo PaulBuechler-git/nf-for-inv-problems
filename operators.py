4b3d0ca14a0992c963d7084432ef82fa7d2aa620
Paul Büchler <paul.buechler@tuhh.de>
2023-10-17 13:39:10 +0200

    created regularizer file and ported operators

---
+++ operators.py
@@ -0,0 +1,42 @@
+import abc
+
+import numpy as np
+import torch
+from torchvision import transforms
+import torch.nn.functional as F
+
+
+class Operator:
+    @abc.abstractmethod
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError('Operator has to be callable')
+
+
+class NoiseOperator:
+    def __init__(self, mean=0, std=1, device='cpu'):
+        self.std = std
+        self.mean = mean
+        self.device = device
+
+    def __call__(self, img):
+        noise = torch.tensor(np.random.normal(self.mean, self.std, img.size()), device=self.device,
+                             dtype=torch.float)
+        return torch.clamp(img + noise * 1 / 255., min=0, max=1)
+
+
+def append_noise(img, mean=0, std=1, device='cpu'):
+    noise = torch.tensor(np.random.normal(mean, std, img.size()), device=device, dtype=torch.float) / 255.
+    return torch.clamp(img + noise, min=0, max=1)
+
+
+class BlurOperator(Operator):
+    def __init__(self, kernel, device='cpu'):
+        self.device = device
+        self.kernel = kernel
+        self.kernel_size = kernel.squeeze().size(0)
+
+    def __call__(self, image, padding=True):
+        if padding:
+            pad_trans = transforms.Pad([self.kernel_size // 2, ], padding_mode="reflect")
+            image = pad_trans(image)
+        return F.conv2d(image, self.kernel).to(self.device)
---
+++ regularisers.py
@@ -0,0 +1,47 @@
+import abc
+
+import torch
+from torch import Tensor
+import torch.nn.functional as F
+
+from dataset.FastPatchExtractor import FastPatchExtractor
+
+
+class Regulariser:
+    @abc.abstractmethod
+    def evaluate(self, input: Tensor):
+        raise NotImplementedError('Evaluate not implemented')
+
+    @abc.abstractmethod
+    def get_hparams(self):
+        raise NotImplementedError('Regulariser should return the hyperparameters with this function')
+
+
+class PatchNrRegulariser(Regulariser):
+
+    def __init__(self, flow_model, p_size=6, sample_number=40000, padding=True, padding_size=8, device='cpu'):
+        self.padding = padding
+        self.padding_size = padding_size
+        self.sample_number = sample_number
+        self.flow_model = flow_model
+        self.patch_size = p_size
+        self.patch_extractor = FastPatchExtractor(p_dim=p_size, device=device)
+        self.pad_transformation = F.pad([self.padding_size], mode='reflect')
+
+    def loss(self, batch):
+        self.flow_model.eval()
+        z, z_log_det = self.flow_model(batch, rev=True)
+        return torch.mean(torch.sum(z**2, dim=1)/2) - torch.mean(z_log_det)
+
+    def get_hparams(self):
+        return {
+            "p_size": self.patch_size,
+            "sample_number": self.sample_number,
+            "padding_size": self.padding_size,
+            "padding": self.padding
+        }
+
+    def evaluate(self, img):
+        padded_img = F.pad(img, [self.padding_size], mode='reflect') if self.padding else img
+        batch = self.patch_extractor.extract(padded_img, self.sample_number)
+        return self.loss(batch)
