32d7ce3f9410d8451824be4064c7f9f61b31ea26
Paul Büchler <paul.buechler@tuhh.de>
2023-10-19 00:05:19 +0200

    changes

--- file_utils.py
+++ file_utils.py
@@ -6,11 +6,9 @@ def create_versioned_dir(path, name):
     if not os.path.isdir(path):
         os.mkdir(path)
     folder = os.path.join(path, name)
-    print(folder)
     if not os.path.isdir(folder):
         os.mkdir(folder)
     folders = list(filter(lambda file: re.search(r'version_[0-9]+', file), os.listdir(folder)))
-    print(folders)
     if len(folders) == 0:
         version_dir = os.path.join(folder, 'version_0')
         os.mkdir(version_dir)
@@ -21,3 +19,11 @@ def create_versioned_dir(path, name):
         new_version_dir = os.path.join(folder, f'version_{versions[-1] + 1}')
         os.mkdir(new_version_dir)
         return new_version_dir
+
+
+def get_version_dir(path, name, version):
+    p = os.path.join(path, name, f'version_{version}')
+    if os.path.isdir(p)
+        return p
+    else:
+        raise Exception('path does not exist')
